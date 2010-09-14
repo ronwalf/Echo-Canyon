cb35fc3367f057550ed385bf130be2a4b2535caf
Ron Alford <ronwalf@volus.net>
2010-09-14 12:41:13 -0400

    Echo Bomb 1

--- src/net/volus/ronwalf/phs2010/networking/echocanyon/MessageGenerator.java
+++ src/net/volus/ronwalf/phs2010/networking/echocanyon/MessageGenerator.java
@@ -2,14 +2,16 @@ package net.volus.ronwalf.phs2010.networking.echocanyon;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 
-public class MessageGenerator {
+public class MessageGenerator implements Iterable<String>, Iterator<String> {
 
 	private final List<String> messages;
-	int count;
+	private final int total;
+	private int count;
 	
-	public MessageGenerator() {
+	public MessageGenerator(int total) {
 		messages = new ArrayList<String>(Arrays.asList(
 				"This is the default message.",
 				"It tells me what to say.",
@@ -17,13 +19,34 @@ public class MessageGenerator {
 				"who wrote me up this way."
 		));
 		count = 0;
+		this.total = total;
 	}
 	
 	public String next() {
-		
-		String msg  ="(" + count + ") " + messages.get( count++ % messages.size() );
-		
+		String msg = null;
+
+		// Since no other method modifies count, we 
+		// could have just used the synchronized keyword on the method, ie:
+		// public synchronized String next() {...}
+		synchronized (this) {
+			if (count < total) {
+				msg = "(" + count + ") "
+						+ messages.get(count++ % messages.size());
+			}
+		}
 		return msg;
 	}
-	
+
+	public Iterator<String> iterator() {
+		return this;
+	}
+
+	public boolean hasNext() {
+		return count < total;
+	}
+
+	public void remove() {
+		throw new UnsupportedOperationException("Cannot remove from MessageGenerator!");
+	}
+
 }
